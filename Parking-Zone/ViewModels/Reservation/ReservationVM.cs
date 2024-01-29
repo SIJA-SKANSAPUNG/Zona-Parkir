93afb8089430fbdc002bc11eef178828ff7fd88e
Abdulloh <abdullohprogram@gmail.com>
2024-01-29 19:22:39 +0500

    Reservation View model added

---
+++ Parking-Zone/ViewModels/Reservation/ReservationVM.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace Parking_Zone.ViewModels.Reservation
+{
+    public class ReservationVM
+    {
+        [Required]
+        public int Duration { get; set; }
+        [Required]
+        public DateTime StartTime { get; set; }
+        [Required]
+        public Guid ParkingZoneId { get; set; }
+        public IEnumerable<Models.ParkingSlot> ParkingSlots { get; set; }
+        public SelectList ParkingZones { get; set; }
+    }
+}
